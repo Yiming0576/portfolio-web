5f2fad0ffb6b1a512de954422c2398645e6a832f
Yiming0576 <Yiming0576@gmail.com>
2024-07-04 22:56:49 -0400

    update all of the rfc

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,11 @@
 
 import './App.css'
+import Banner from './components/Banner'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import NavBar from './components/NavBar'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
 
 function App() {
 
